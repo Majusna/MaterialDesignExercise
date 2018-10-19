ad048a53fc1cba2fb1a7a0565d9aaba737d93b7a
piccolaMaja <todorovic.maja@hotmail.com>
2018-10-19 15:45:47 +0200

    menu on toolbar, changing color doesn't work

--- app/src/main/java/com/example/android/materialdesignexercise/MainActivity.java
+++ app/src/main/java/com/example/android/materialdesignexercise/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,4 +31,27 @@ public class MainActivity extends AppCompatActivity {
         imageHolder.setImageDrawable(roundDrawable);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id== R.id.item1_id){
+            Toast.makeText(this, "item one", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (id== R.id.item2_id){
+            Toast.makeText(this, "item two", Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            Toast.makeText(this, "item boli me grlo", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
