c719b576c12781d31a22617b3b2332d189c2f964
piccolaMaja <todorovic.maja@hotmail.com>
2018-10-21 19:45:47 +0200

    navigation drawer 4

--- app/src/main/java/com/example/android/materialdesignexercise/MainActivity.java
+++ app/src/main/java/com/example/android/materialdesignexercise/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.android.materialdesignexercise;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +17,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     android.support.v7.widget.Toolbar mToolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -38,12 +40,24 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,mToolbar,R.string.open_drawer,R.string.close_drawer);
         drawerLayout.setDrawerListener(toggle);
 
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         ImageView imageHolder = findViewById(R.id.image_holder);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.image_decoration);
         RoundedBitmapDrawable roundDrawable = RoundedBitmapDrawableFactory.create(getResources(),bitmap);
         roundDrawable.setCircular(true);
         imageHolder.setImageDrawable(roundDrawable);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
     }
 
     @Override
@@ -80,5 +94,33 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    // difference between group and an item:
+    //when item is defined in navigation bar, it acts like a button (last 3 items),
+    //whe item is deffined in a group, that item will have a hold characteristics (first 4 items)
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.inbox_id:
+                break;
+            case R.id.starred_id:
+                break;
+            case R.id.sent_id:
+                break;
+            case R.id.drafts_id:
+                break;
+            case R.id.all_mail_id:
+                break;
+            case R.id.trash_id:
+                break;
+            case R.id.spam_id:
+                break;
+
+        }
+
+        return true;
     }
 }
